c47cba76f92e71a1ff6689688fa299be164c4798
Turtle <emanuelsadhot@gmail.com>
2022-09-07 20:31:20 -0300

    update fundo

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -12,7 +12,7 @@ export default function Home() {
         <title>Turtle</title>
       </Head>
     <div className="flex-col">
-        <div className="bg-[url('/flowers.gif')] rounded-t-2xl pr-[2px] h-[120px]">
+        <div className="bg-[url('/fundo.gif')] rounded-t-2xl pr-[2px] h-[120px]">
           <div className="text-center pl-5 pr-40 pt-20">
             <img className="rounded-full border-8 border-zinc-900 h-24" src="/icon.gif" alt="icon" />
           </div>
@@ -20,12 +20,12 @@ export default function Home() {
           <div className="pl-4 pt-[58px] pr-4 bg-zinc-900 rounded-b-2xl">
             <h1 className="text-white text-lg"><strong>Emanuel Santos</strong></h1>
             <p className="text-zinc-400 pb-4 text-sm"><strong>Tutlee#3006</strong></p>
-                <div className="pt-8 border-t border-zinc-800">
+                <div className="pt-4 border-t border-zinc-800">
                   <p className="text-gray-200 text-xs pb-2"><strong>ABOUT ME</strong></p>
                   <p className="span text-white text-xs">Front End Developer<br /></p>
                   <span className=" bg-zinc-800 px-2 py-1 rounded-md text-zinc-300 font-mono text-[11px]">Only know that i know nothing.</span>
                 </div>
-                <div className="pt-8 pb-4">
+                <div className="pt-4 pb-4">
                   <p className="text-white text-xs"><strong>ROLES</strong></p>
                   <div className="flex gap-1 pt-1">
                     <span className="text-zinc-300 text-xs bg-zinc-800 rounded-sm px-2 py-0.5">🟡 Javascript</span>
