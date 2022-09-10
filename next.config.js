c93e5364ec28d1cba434d2b7caeb25a720bb8a84
Turtle <emanuelsadhot@gmail.com>
2022-09-10 10:02:30 -0300

    update redirects

--- next.config.js
+++ next.config.js
@@ -8,12 +8,13 @@ const nextConfig = {
   experimental: {
     images: {
       allowFutureImage: true
-    },
+    }
+  },
     async redirects() {
       return [
         {
           source: "/github",
-          destination: "https://github.com/turtle071",
+          destination: "https://github.com/turtle071/",
           permanent: false
         },
         {
@@ -21,9 +22,18 @@ const nextConfig = {
           destination: "https://discord.com/users/335411366041092096",
           permanent: false
         },
+        {
+          source: "/contact",
+          destination: "mailto:support@turtles.studio",
+          permanent: false
+        },
+        {
+          source: "/repositories",
+          destination: "https://github.com/turtle071?tab=repositories",
+          permanent: false
+        },
       ];
     }
-  },
 }
 
 module.exports = nextConfig
